7e44ad9703c6f8003d5bd87aeed76d737c44514a
Ivan_Sytnychenko <sytnychenkoivan@gmail.com>
2025-03-25 00:35:22 +0200

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,198 @@
+// import "../scss/style.scss";
+
+import * as flsFunctions from "./modules/functions.js";
+
+flsFunctions.isWebp();
+
+
+// import Swiper, { Navigation, Pagination } from 'swiper';
+
+// const swiper = new Swiper(...);
+
+
+// import "./modules/script.js";
+
+// import "./modules/dynamic_adapt.js";
+
+flsFunctions.menuInit();
+
+// flsFunctions.ibg();
+
+// import "./libs/fullpage.js";
+
+// import "./files/sliders.js";
+
+// flsFunctions.tabs();
+
+// import "./modules/-ibg.js";
+/*
+Модуль "Спойлери"
+Документація: https://template.fls.guru/template-docs/modul-spojlery.html
+Сніппет (HTML): spollers
+*/
+flsFunctions.spollers();
+
+/*
+Модуль "Таби"
+Документація: https://template.fls.guru/template-docs/modul-taby.html
+Сніппет (HTML): tabs
+*/
+// flsFunctions.tabs();
+
+/*
+Модуль "Показати ще"
+Документація: https://template.fls.guru/template-docs/modul-pokazat-eshhjo.html
+Сніппет (HTML): showmore
+*/
+// flsFunctions.showMore();
+
+/*
+Модуль "Ефект хвиль"
+Документация: https://template.fls.guru/template-docs/modul-ripple-effect.html
+Сниппет (HTML):
+*/
+// flsFunctions.rippleEffect();
+
+/*
+Модуль "Кастомний курсор"
+Документация:
+Сниппет (HTML):
+*/
+// flsFunctions.customCursor(true);
+
+/*
+Модуль "Попапи"
+Документація: https://template.fls.guru/template-docs/funkcional-popup.html
+Сніппет (HTML): pl, pop
+*/
+// import './libs/popup.js'
+
+/*
+Модуль паралаксу мишею
+Документація: https://template.fls.guru/template-docs/modul-animacii-parallaks-obektov-pri-dvizhenii-myshi.html
+*/
+import './libs/parallax-mouse.js'
+
+// ========================================================================================================================================================================================================================================================
+// Робота з формами ========================================================================================================================================================================================================================================================
+// ========================================================================================================================================================================================================================================================
+import * as flsForms from "./files/forms/forms.js";
+
+/* Робота з полями форми */
+/* Документація: https://template.fls.guru/template-docs/rabota-s-formami.html */
+
+flsForms.formFieldsInit({
+	viewPass: false,
+	autoHeight: false
+});
+
+/* Надсилання форми */
+/* Документація: https://template.fls.guru/template-docs/rabota-s-formami.html */
+flsForms.formSubmit();
+
+/* Модуль форми "кількість" */
+// flsForms.formQuantity();
+
+/* Модуль зіркового рейтингу */
+// flsForms.formRating();
+
+/* Модуль роботи з select. */
+// import './libs/select.js'
+
+/* Модуль роботи з календарем */
+// import './files/forms/datepicker.js'
+
+/* (У роботі) Модуль роботи з масками.*/
+/*
+Підключення та налаштування виконується у файлі js/files/forms/inputmask.js
+Документація по роботі у шаблоні:
+Документація плагіна: https://github.com/RobinHerbots/inputmask
+Сніппет(HTML):
+*/
+// import "./files/forms/inputmask.js";
+
+/* Модуль роботи з повзунком */
+/*
+Підключення та налаштування виконується у файлі js/files/forms/range.js
+Документація по роботі у шаблоні:
+Документація плагіна: https://refreshless.com/nouislider/
+Сніппет (HTML): range
+*/
+// import "./files/forms/range.js";
+
+/* Модуль роботи з підказками (tippy) */
+/*
+Підключення плагіна Tippy.js та налаштування виконується у файлі js/files/tippy.js
+Документація по роботі у шаблоні:
+Документація плагіна: https://atomiks.github.io/tippyjs/
+Сніппет (HTML): tip (додає атрибут з підказкою для html тега)
+*/
+// import "./files/tippy.js";
+
+// ========================================================================================================================================================================================================================================================
+// Модулі роботи з прокручуванням сторінки ========================================================================================================================================================================================================================================================
+// ========================================================================================================================================================================================================================================================
+
+/*
+Зміна дизайну скроллбару
+Документація по роботі у шаблоні: У HTML додаємо до блоку атрибут data-simplebar
+Документація плагіна: https://github.com/Grsmto/simplebar/tree/master/packages/simplebar
+Сніппет(HTML): 
+*/
+// import './files/scroll/simplebar.js';
+
+// Ліниве (відкладене) завантаження картинок
+// Документація по роботі у шаблоні: https://template.fls.guru/template-docs/modul-lenivaya-podgruzka-lazy-loading.html
+// Документація плагіна: https://github.com/verlok/vanilla-lazyload
+// Сніппет(HTML):
+// import './files/scroll/lazyload.js';
+
+// Спостерігач за об'єктами з атрибутом data-watch
+// Документація: https://template.fls.guru/template-docs/modul-nabljudatel-za-poyavleniem-elementa-pri-skrolle.html
+// Сніппет(HTML):
+// import './libs/watcher.js'
+
+// Модуль поекранної прокрутки
+// Документація: https://template.fls.guru/template-docs/modul-poekrannoj-prokrutki-stranicy-fullpage.html
+// Сніппет(HTML):
+// import './libs/fullpage.js'
+
+// Модуль паралаксу
+// Документація: https://template.fls.guru/template-docs/paralaks-pri-skroli.html
+// Сніппет(HTML):
+import './libs/parallax.js'
+
+// Функції роботи скролом
+import * as flsScroll from "./files/scroll/scroll.js";
+
+// Плавна навігація по сторінці
+// Документація: https://template.fls.guru/template-docs/modul-plavnoj-navigacii-po-stranice.html
+// flsScroll.pageNavigation();
+
+// Функціонал додавання класів до хедеру під час прокручування
+// Документація: https://template.fls.guru/template-docs/modul-dobavleniya-klassov-k-shapke-pri-prokrutke-stranicy.html
+flsScroll.headerScroll();
+
+// Модуль анімація цифрового лічильника
+// Документація: https://template.fls.guru/template-docs/modul-animacii-cifrovogo-lichilnika.html
+// Сніппет(HTML):
+// flsScroll.digitsCounter();
+
+
+
+// ========================================================================================================================================================================================================================================================
+// Інші плагіни ============================================================================================================================================================================================================================================================================================================
+// ========================================================================================================================================================================================================================================================
+
+/* Динамічний адаптив */
+// Документація: https://template.fls.guru/template-docs/dinamicheskij-adaptiv.html
+import "./libs/dynamic_adapt.js";
+
+/* Форматування чисел */
+// import './libs/wNumb.min.js';
+
+// ========================================================================================================================================================================================================================================================
+// Інше ========================================================================================================================================================================================================================================================
+// ========================================================================================================================================================================================================================================================
+/* Підключаємо файли зі своїм кодом */
+import "./files/script.js";
---
+++ src/js/modules/script.js
@@ -0,0 +1,13 @@
+// let user_icon = document.querySelector('.user-header__icon');
+// let user_menu = document.querySelector('.user-header__menu');
+// user_icon.addEventListener("click", function (e) {
+// 	user_menu.classList.toggle('_active');
+// });
+
+// document.documentElement.addEventListener("click", function (e) {
+// 	if (!e.target.closest('.user-header')) {
+// 		user_menu.classList.remove('_active');
+// 	}
+// });
+
+
